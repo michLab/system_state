080bf87091bb7dea8700cdc6c634b2257a31a5d6
Michal Labowski <michal.labowski@gmail.com>
2019-08-12 18:55:08 +0200

    fix: Missing "self" in calling queue in sys class

--- main.py
+++ main.py
@@ -2,72 +2,8 @@ import logging
 from logger import Logger
 from systemStatus import SystemStatus
 import threading
-from multiprocessing import Queue
+from queue import Queue
 from time import sleep, time
-
-
-class SystemStatus1:
-    def __init__(self, logger, q_cpu, q_ap, q_gcs):
-        self.logger = logger
-        self.queue_cpu = q_cpu
-        self.queue_ap = q_ap
-        self.queue_gcs = q_gcs
-
-        self.last_gcs_message_time = 0
-        self.gcs_message_timeout = 1.5
-        self.gcs_timeout_exceeded = False
-
-        self.last_ap_message_time = 0
-        self.ap_message_timeout = 1.5
-        self.ap_timeout_exceeded = False
-        
-        logger.info("[SYS_STATE]: logger.id = %d", id(self.logger))
-        logger.info("[SYS_STATE]: queue_ap.id = %d", id(self.queue_ap))
-        logger.info("[SYS_STATE]: queue_gcs.id = %d", id(self.queue_gcs))
-
-
-    def update(self):
-        self.update_gcs()
-        self.update_ap()
-        self.logger.info("[SYS_STATE]: gcs_mps = %d, ap_mps = %d",
-            self.gcs_msg_count, self.ap_msg_count)
-        sleep(0.5)
-
-    def update_gcs(self):
-        try:
-            self.gcs_msg_count = queue_gcs.get_nowait()
-            self.logger.debug("[SYS_STATE]: get gcs_msg_count = %d", self.gcs_msg_count)
-            self.last_gcs_message_time = self.get_time()
-            self.gcs_timeout_exceeded = False
-
-        except:
-            self.logger.debug("[SYS_STATE]: No data in queue_gcs")
-            if (self.get_time() - self.last_gcs_message_time) > self.gcs_message_timeout:
-                self.logger.warning("[SYS_STATE]: Last gcs data is older than %f [s]",
-                    self.gcs_message_timeout)
-                self.gcs_timeout_exceeded = True
-                self.gcs_msg_count = 0
-
-    
-    def update_ap(self):
-        try:
-            while True:  # Get only the last message, drop others
-                self.ap_msg_count = queue_ap.get_nowait()
-                self.logger.debug("[SYS_STATE]: get ap_msg_count = %d", self.ap_msg_count)
-                self.last_ap_message_time = self.get_time()
-                self.ap_timeout_exceeded = False
-
-        except:
-            self.logger.debug("[SYS_STATE]: No data in queue_ap")
-            if (self.get_time() - self.last_ap_message_time) > self.ap_message_timeout:
-                self.logger.warning("[SYS_STATE]: Last ap data is older than %f [s]",
-                    self.ap_message_timeout)
-                self.ap_timeout_exceeded = True
-                self.ap_msg_count = 0
-
-        
-    def get_time(self):
-        return time()
 
 
 def system_state_callback(logger, q_cpu, q_ap, q_gcs):
@@ -112,12 +48,7 @@ if __name__ == "__main__":
     queue_cpu = Queue()
     queue_ap = Queue()
     queue_gcs = Queue()
-
-    logger.info("[MAIN]: logger.id = %d", id(logger))
-    logger.info("[MAIN]: queue_ap.id = %d", id(queue_ap))
-    logger.info("[MAIN]: queue_gcs.id = %d", id(queue_gcs))
-
-    
+  
     # Create threads:
     thread_system_state = threading.Thread(target=system_state_callback, 
         args=(logger, queue_cpu, queue_ap, queue_gcs,), daemon=True)
--- systemStatus.py
+++ systemStatus.py
@@ -1,4 +1,5 @@
 from time import time, sleep
+import queue
 
 
 class SystemStatus:
@@ -7,10 +8,6 @@ class SystemStatus:
         self.queue_cpu = q_cpu
         self.queue_ap = q_ap
         self.queue_gcs = q_gcs
-
-        logger.info("[SYS_STATE]: logger.id = %d", id(self.logger))
-        logger.info("[SYS_STATE]: queue_ap.id = %d", id(self.queue_ap))
-        logger.info("[SYS_STATE]: queue_gcs.id = %d", id(self.queue_gcs))
 
         self.last_gcs_message_time = 0
         self.gcs_message_timeout = 1.5
@@ -22,15 +19,16 @@ class SystemStatus:
         
 
     def update(self):
-        self.update_gcs()
+        self.update_gcs()    
         self.update_ap()
         self.logger.info("[SYS_STATE]: gcs_mps = %d, ap_mps = %d",
             self.gcs_msg_count, self.ap_msg_count)
         sleep(0.5)
 
+        
     def update_gcs(self):
         try:
-            self.gcs_msg_count = queue_gcs.get_nowait()
+            self.gcs_msg_count = self.queue_gcs.get_nowait()
             self.logger.debug("[SYS_STATE]: get gcs_msg_count = %d", self.gcs_msg_count)
             self.last_gcs_message_time = self.get_time()
             self.gcs_timeout_exceeded = False
@@ -47,7 +45,7 @@ class SystemStatus:
     def update_ap(self):
         try:
             while True:  # Get only the last message, drop others
-                self.ap_msg_count = queue_ap.get_nowait()
+                self.ap_msg_count = self.queue_ap.get_nowait()
                 self.logger.debug("[SYS_STATE]: get ap_msg_count = %d", self.ap_msg_count)
                 self.last_ap_message_time = self.get_time()
                 self.ap_timeout_exceeded = False
